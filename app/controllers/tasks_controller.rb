d95b7bc63d70d3286ba62eeef2cb747d146ffe91
yuya.kudo <yuyaaa.1203@gmail.com>
2019-03-10 04:06:48 +0000

    authenticate login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,13 @@
 class TasksController < ApplicationController
     before_action :set_task, only: [:show, :edit, :update, :destroy]
+    before_action :require_user_logged_in
+    
     def index
-        @tasks = Task.all
+        #@tasks = Task.all
+        if logged_in?
+            @task = current_user.task.build
+            @tasks = current_user.task.order('created_at DESC')
+        end
     end
     
     def show
@@ -12,12 +18,14 @@ class TasksController < ApplicationController
     end
     
     def create
-        @task = Task.new(task_params)
+        # @task = Task.new(task_params)
+        @task = current_user.task.build(task_params)
         
         if @task.save
             flash[:success] = 'Task が正常に作成されました'
             redirect_to @task
         else
+            @tasks = current_user.task.order('created_at DESC')
             flash.now[:danger] = 'Task が作成されませんでした'
             render :new
         end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   root to: 'tasks#index'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
   
   resources :tasks
 end
